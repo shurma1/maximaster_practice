f343c0f4db4f9e8dec1c45a210f8959ba24e223e
shurma1 <dmitriy.prokhorov.04@mail.ru>
2025-04-02 12:04:43 +0300

    some changes

--- src/pages/RandomColor/RandomColor.tsx
+++ src/pages/RandomColor/RandomColor.tsx
@@ -1,13 +1,8 @@
-import {Stack} from "@mui/material";
-import Input from '@mui/material/Input';
+
 
 const RandomColor = () => {
 	return (
-		<Stack>
-			<Input
-				placeholder="123"
-			/>
-		</Stack>
+		<div></div>
 	);
 };
 
